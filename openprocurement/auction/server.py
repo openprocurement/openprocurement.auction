a194c8db43d81aa793254dd53c4d9c796fb2cc92
Taras Kozlovskyi <ktarasz@quintagroup.com>
2014-11-27 18:33:18 +0200

    Added SSE endpoint for logged users

--- openprocurement/auction/server.py
+++ openprocurement/auction/server.py
@@ -1,11 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response, abort
 from gevent.wsgi import WSGIServer
 from datetime import datetime
 from pytz import timezone
 from openprocurement.auction.forms import BidsForm
+from gevent.queue import Queue
+from .utils import prepare_sse_msg
+import json
 
 
 app = Flask(__name__, static_url_path='', template_folder='static')
+app.auction_bidders = {}
+
+
+def event_sub(bidder_id, hash_id):
+    while True:
+        msg = app.auction_bidders[bidder_id][hash_id].get()
+        yield prepare_sse_msg('new_user', json.dumps(msg))
 
 
 @app.route('/postbid', methods=['POST'])
@@ -27,10 +37,27 @@ def postBid():
         return jsonify(response)
 
 
+@app.route("/event_source")
+def event_source():
+    if request.args.get('hash') and request.args.get('bidder_id'):
+        if request.args.get('bidder_id') not in app.auction_bidders:
+            app.auction_bidders[request.args.get('bidder_id')] = {}
+        if request.args.get('hash') not in app.auction_bidders[request.args.get('bidder_id')]:
+            app.auction_bidders[request.args.get('bidder_id')][request.args.get('hash')] = Queue()
+        new_client = {'ip': request.headers.get('X-Forwarded-For'),
+                      'User-Agent': request.headers.get('User-Agent'),
+                      'client_hash': request.args.get('hash')}
+        for keys in app.auction_bidders[request.args.get('bidder_id')]:
+            if keys != request.args.get('hash'):
+                app.auction_bidders[request.args.get('bidder_id')][keys].put(new_client)
+
+        return Response(event_sub(request.args.get('bidder_id'), request.args.get('hash')), mimetype="text/event-stream")
+    else:
+        return abort(404)
+
+
 def run_server(auction):
     app.config['auction'] = auction
     server = WSGIServer((auction.host, auction.port, ), app)
     server.start()
     return server
-
-
