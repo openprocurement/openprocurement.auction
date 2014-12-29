fe72669aca304fd919913202bf84c10b64b83b69
Taras Kozlovskyi <ktarasz@quintagroup.com>
2014-12-29 16:27:27 +0200

    JShint app.js

--- openprocurement/auction/static/static/js/app.js
+++ openprocurement/auction/static/static/js/app.js
@@ -3,24 +3,29 @@ var db = {};
 var bidder_id = "0"
 
 app.constant('AuctionConfig', {
-  auction_doc_id: auction_doc_id,
-  remote_db: db_url,
-  restart_retries: 10,
-  default_lang: 'uk',
-  debug: true
+    auction_doc_id: auction_doc_id,
+    remote_db: db_url,
+    restart_retries: 10,
+    default_lang: 'uk',
+    debug: true
 });
 
 app.filter('formatnumber', ['$filter',
-  function(filter) {
-    return function(val) {
-      return (filter('number')(val) || "").replace(/,/g, " ") || undefined;
+    function(filter) {
+        return function(val) {
+            return (filter('number')(val) || "").replace(/,/g, " ") || undefined;
+        }
     }
-  }
 ]);
 
-app.config(['$logProvider', 'AuctionConfig', 'growlProvider', function($logProvider, AuctionConfig, growlProvider) {  
-   $logProvider.debugEnabled(AuctionConfig.debug); // default is true
-   growlProvider.globalTimeToLive({success: 4000, error: 10000, warning: 10000, info: 4000});
-   growlProvider.globalPosition('top-center');
-   growlProvider.onlyUniqueMessages(false);
+app.config(['$logProvider', 'AuctionConfig', 'growlProvider', function($logProvider, AuctionConfig, growlProvider) {
+    $logProvider.debugEnabled(AuctionConfig.debug); // default is true
+    growlProvider.globalTimeToLive({
+        success: 4000,
+        error: 10000,
+        warning: 10000,
+        info: 4000
+    });
+    growlProvider.globalPosition('top-center');
+    growlProvider.onlyUniqueMessages(false);
 }]);
