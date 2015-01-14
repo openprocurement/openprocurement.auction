9d8fdad3543b7e95e2bcd40fba49ae3ecccae476
Taras Kozlovskyi <ktarasz@quintagroup.com>
2015-01-14 17:39:10 +0200

    Added parse and stringify Query strings functions

--- openprocurement/auction/static/static/js/utils.js
+++ openprocurement/auction/static/static/js/utils.js
@@ -187,13 +187,56 @@ angular.module('auction')
         }
       }, 0);
     }
+
+    function parseQueryString(str) {
+      if (typeof str !== 'string') {
+        return {};
+      }
+
+      str = str.trim().replace(/^(\?|#)/, '');
+
+      if (!str) {
+        return {};
+      }
+
+      return str.trim().split('&').reduce(function (ret, param) {
+        var parts = param.replace(/\+/g, ' ').split('=');
+        var key = parts[0];
+        var val = parts[1];
+        key = decodeURIComponent(key);
+        val = val === undefined ? null : decodeURIComponent(val);
+        if (!ret.hasOwnProperty(key)) {
+          ret[key] = val;
+        } else if (Array.isArray(ret[key])) {
+          ret[key].push(val);
+        } else {
+          ret[key] = [ret[key], val];
+        }
+        return ret;
+      }, {});
+    }
+
+    function stringifyQueryString(obj) {
+      return obj ? Object.keys(obj).map(function (key) {
+        var val = obj[key];
+        if (Array.isArray(val)) {
+          return val.map(function (val2) {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(val2);
+          }).join('&');
+        }
+        return encodeURIComponent(key) + '=' + encodeURIComponent(val);
+      }).join('&') : '';
+    }
+
     return {
       'prepare_info_timer_data': prepare_info_timer_data,
       'prepare_progress_timer_data': prepare_progress_timer_data,
       'get_bidder_id': get_bidder_id,
       'format_date': format_date,
       'get_round_data': get_round_data,
-      'scroll_to_stage': scroll_to_stage
+      'scroll_to_stage': scroll_to_stage,
+      'parseQueryString': parseQueryString,
+      'stringifyQueryString': stringifyQueryString
     };
   }]);
 
