1676512670ca46155448aea87d6c86d65a50a23f
Leits <leits@quintagroup.org>
2016-06-16 14:23:23 +0300

    Add reshedule auction command

--- openprocurement/auction/static/static/js/translations.js
+++ openprocurement/auction/static/static/js/translations.js
@@ -83,7 +83,8 @@ angular.module('auction')
       'to General': 'to General',
       'The proposal you have submitted coincides with a proposal of the other participant. His proposal will be considered first, since it has been submitted earlier.': 'The proposal you have submitted coincides with a proposal of the other participant. His proposal will be considered first, since it has been submitted earlier.',
       'Waiting for the disclosure of the participants\' names': 'Waiting for the disclosure of the participants\' names',
-      'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.'
+      'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.',
+      'Auction has not started and will be rescheduled': 'Auction has not started and will be rescheduled'
     });
 
     $translateProvider.translations('uk', {
@@ -168,7 +169,8 @@ angular.module('auction')
       'to General': 'до Загальної',
       'The proposal you have submitted coincides with a proposal of the other participant. His proposal will be considered first, since it has been submitted earlier.': 'Подана вами пропозиція співпадає з пропозицією іншого учасника. Його пропозиція розглядатиметься першою, оскільки вона подана раніше.',
       'Waiting for the disclosure of the participants\' names': 'Очікуємо на розкриття імен учасників',
-      'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Не вдається зробити ставку. Перевірте, що в переглядачі відкрито не більше 2-ох аукціонів.'
+      'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Не вдається зробити ставку. Перевірте, що в переглядачі відкрито не більше 2-ох аукціонів.',
+      'Auction has not started and will be rescheduled': 'Аукціон не розпочався і буде перепланований'
     });
 
 
@@ -254,6 +256,7 @@ angular.module('auction')
       'to General': 'к Общей',
       'The proposal you have submitted coincides with a proposal of the other participant. His proposal will be considered first, since it has been submitted earlier.': 'Поданное вами предложение совпадает с предложением другого участника. Его предложение будет рассматриваться первым, поскольку оно подано раньше.',
       'Waiting for the disclosure of the participants\' names': 'Ожидаем раскрытия имен участников',
-      'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Невозможно сделать ставку. Проверьте, что в браузере открыто не более 2-х аукционов.'
+      'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Невозможно сделать ставку. Проверьте, что в браузере открыто не более 2-х аукционов.',
+      'Auction has not started and will be rescheduled': 'Аукцион не начался и будет перепланирован'
     });
   }]);
