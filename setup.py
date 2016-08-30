acf866f06bcda31860620dfd7653212cdb70e080
Taras Kozlovskyi <ktarasz@quintagroup.com>
2016-08-30 15:25:31 +0300

    Prepare openprocurement.auction 2.1.0.dev0+auction.worker.sharding.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0.4.dev5+auction.main.sharding'
+version = '2.1.0.dev0+auction.worker.sharding'
 
 
 setup(name='openprocurement.auction',
