1e0f7197cbb8e1b0b65722243b8fe95958393cc2
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-06-16 18:54:33 +0300

    Added constants

---
+++ openprocurement/auction/constants.py
@@ -0,0 +1,8 @@
+DEFAULT_PROCUREMENT_METHOD_TYPES = [
+    'belowThreshold',
+    'aboveThresholdUA',
+    'aboveThresholdEU',
+    'competitiveDialogueEU.stage2',
+    'competitiveDialogueUA.stage2',
+    'aboveThresholdUA.defense'
+]
