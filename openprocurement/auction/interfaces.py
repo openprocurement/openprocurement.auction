de7ae696ffc3373c93284d7665812104fe3cd5d1
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-06-16 18:19:47 +0300

    Added missing interfaces

---
+++ openprocurement/auction/interfaces.py
@@ -0,0 +1,17 @@
+from zope.interface import Interface
+
+
+class IComponents(Interface):
+    """"""
+
+
+class IAuctionDatabridge(Interface):
+    """"""
+
+
+class IWorker(Interface):
+    """"""
+
+
+class IAuctionsRunner(Interface):
+    """"""
