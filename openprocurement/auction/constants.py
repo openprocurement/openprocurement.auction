89b6b94550480688aa8c7bfe63f50521d86ac16a
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-07-04 19:01:15 +0300

    Remove constants.py

--- openprocurement/auction/constants.py
+++
@@ -1,8 +0,0 @@
-DEFAULT_PROCUREMENT_METHOD_TYPES = [
-    'belowThreshold',
-    'aboveThresholdUA',
-    'aboveThresholdEU',
-    'competitiveDialogueEU.stage2',
-    'competitiveDialogueUA.stage2',
-    'aboveThresholdUA.defense'
-]
