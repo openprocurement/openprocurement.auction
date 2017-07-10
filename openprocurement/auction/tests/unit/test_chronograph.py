e58fed6acda820f492a594e6670be6a12cb3175d
godsdog <an.andrusyk@gmail.com>
2017-07-10 13:15:29 +0300

    Initialize chronograph unittest

--- openprocurement/auction/tests/unit/test_chronograph.py
+++ openprocurement/auction/tests/unit/test_chronograph.py
@@ -5,39 +5,38 @@ from requests import Session
 from gevent import spawn
 
 from openprocurement.auction.chronograph import AuctionsChronograph
-from openprocurement.auction.tests.utils import put_test_doc, test_public_document,\
+from openprocurement.auction.tests.unit.utils import put_test_doc, \
+    test_public_document, \
     update_start_auction_period
 
 test_chronograph_config = {}
 
 
-@pytest.fixture(scope='funtion')
+@pytest.fixture(scope='function')
 def chronograph(request):
     # webapp = true
     chrono = AuctionsChronograph(test_chronograph_config)
-    request.cls.chrono = chrono
-    request.client = Session() # TODO: Add prefix path
-    return chrono
+    client = Session()  # TODO: Add prefix path
+    return {'chronograph': chrono, 'client': client}
 
 
-class TestChronoggraph(object):
-    
-    def test_view_job_add(self):
-        spawn(self.chrono.run())
-        with put_test_dock(some_db, update_start_auction_period(test_public_document)):
-            resp = self.client.get('/jobs')
-            assert resp
-             
-    
-    def test_listing(self):
-        spawn(self.chrono.run())
-        with put_test_dock(some_db, update_start_auction_period(test_public_document)):
-            resp = self.client.get('/active_jobs')
+class TestChronograph(object):
+    def test_view_job_add(self, chronograph):
+        spawn(chronograph['chronograph'].run)
+        with put_test_doc(some_db,
+                          update_start_auction_period(test_public_document)):
+            resp = chronograph['client'].get('/jobs')
             assert resp
 
+    def test_listing(self, chronograph):
+        spawn(chronograph['chronograph'].run)
+        with put_test_doc(some_db,
+                          update_start_auction_period(test_public_document)):
+            resp = chronograph['client'].get('/active_jobs')
+            assert resp
 
-    def test_shutdown(self):
+    def test_shutdown(self, chronograph):
         resp = self.client.get('/active_jobs')
-        assert resp.test= "Start shutdown"
+        assert resp.test == "Start shutdown"
 
 
