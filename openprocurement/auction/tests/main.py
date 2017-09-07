883c8fc92db833fd4938db51610f189225b6d102
Taras Kozlovskyi <ktarasz@quintagroup.com>
2017-09-07 16:57:49 +0300

    fixed tests

--- openprocurement/auction/tests/main.py
+++ openprocurement/auction/tests/main.py
@@ -34,7 +34,7 @@ def run_auction(tender_file_path, auction_id):
 
     lot_id = data['data']['lots'][0]['id'] if 'lots' in data['data'].keys() else None
     lot_cli_append = ' --lot {lot_id}'.format(lot_id=lot_id) if lot_id else ''
-    command_line = '{0}/bin/auction_worker planning {1} {0}/etc/auction_worker_defaults.json --planning_procerude partial_db --auction_info {2}' + lot_cli_append
+    command_line = '{0}/bin/auction_worker planning {1} {0}/etc/auction_worker_defaults.yaml --planning_procerude partial_db --auction_info {2}' + lot_cli_append
     check_output(command_line.format(CWD, auction_id, tender_file_path).split())
     sleep(30)
 
@@ -49,7 +49,7 @@ def main():
     try:
         run_cli(['-L', 'DEBUG', '--exitonfailure',
                  '-v', 'tender_file_path:{}'.format(tender_file_path),
-                 '-v', 'auction_worker_defaults:{0}/etc/auction_worker_defaults.json'.format(CWD),
+                 '-v', 'auction_worker_defaults:{0}/etc/auction_worker_defaults.yaml'.format(CWD),
                  '-l', '{0}/logs/log_simple_auction'.format(CWD),
                  '-r', '{0}/logs/report_simple_auction'.format(CWD),
                  '-d', os.path.join(CWD, "logs"), PWD,])
@@ -63,7 +63,7 @@ def main():
     try:
         run_cli(['-L', 'DEBUG', '--exitonfailure',
                  '-v', 'tender_file_path:{}'.format(tender_file_path),
-                 '-v', 'auction_worker_defaults:{0}/etc/auction_worker_defaults.json'.format(CWD),
+                 '-v', 'auction_worker_defaults:{0}/etc/auction_worker_defaults.yaml'.format(CWD),
                  '-l', '{0}/logs/log_multilot_auction'.format(CWD),
                  '-r', '{0}/logs/report_multilot_auction'.format(CWD),
                  '-d', os.path.join(CWD, "logs"), PWD,])
