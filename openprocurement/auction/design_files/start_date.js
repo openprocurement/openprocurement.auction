f3baaf06446a11db04c7cd49ce3c86e996ba8ab3
yarsanich <nezn27@gmail.com>
2017-11-07 18:12:53 +0200

    update procurementMethodType default

--- openprocurement/auction/design_files/start_date.js
+++ openprocurement/auction/design_files/start_date.js
@@ -2,7 +2,7 @@ function(doc){
   if ((doc.current_stage||0) == -1) {
      var mode = doc.mode||false ? doc.mode : "";
      var api_version = (doc.TENDERS_API_VERSION||false) ? doc.TENDERS_API_VERSION : null;
-     var procurement_method_type = doc.procurementMethodType||null;
+     var procurement_method_type = doc.procurementMethodType|| "";
      var auction_type = (doc.auction_type||false) ? doc.auction_type : "default";
      emit(doc._local_seq, {
          "start": doc.stages[0].start,
