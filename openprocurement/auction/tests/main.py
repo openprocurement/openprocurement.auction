76dafe407037963cf9025f94a3c0933d7f670e40
godsdog <an.andrusyk@gmail.com>
2017-12-06 23:23:55 +0200

    Add possibility run rests on remote server

--- openprocurement/auction/tests/main.py
+++ openprocurement/auction/tests/main.py
@@ -7,29 +7,65 @@ import os.path
 from robot import run_cli
 from pkg_resources import iter_entry_points
 from gevent.subprocess import sleep
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-TESTS = {}
 PWD = os.path.dirname(os.path.realpath(__file__))
 CWD = os.getcwd()
 
 
 def main():
+    TESTS = {}
     for entry_point in iter_entry_points('openprocurement.auction.robottests'):
         suite = entry_point.load()
-        suite(TESTS)
+        TESTS.update(suite())
 
     parser = argparse.ArgumentParser('Auction test runner')
     parser.add_argument('suite',
                         choices=TESTS.keys(),
                         default='simple',
+                        nargs='?',
                         help='test_suite')
+
+    parser.add_argument('--browser',
+                        dest='browser',
+                        choices=['firefox', 'chrome', 'phantomjs'],
+                        default='chrome',
+                        nargs='?',
+                        help='supported browsers')
+
+    parser.add_argument('--ip',
+                        dest='ip',
+                        nargs='?',
+                        help='ip of the remote server where tests will be run')
+
+    parser.add_argument('--port',
+                        dest='port',
+                        nargs='?',
+                        help='port of the remote server where tests will be '
+                             'run')
+
     args = parser.parse_args()
-    test = TESTS.get(args.suite)
 
-    test['runner'](test['tender_file_path'], test['auction_id'])
+    if args.port and (not args.ip):
+        parser.error('The --port argument requires the --ip')
 
-    auction_worker_defaults = test.get('auction_worker_defaults')
+    if args.ip and (args.browser != 'chrome'):
+        parser.error('Only chrome is allowed for remote test running')
+
+    port = getattr(args, 'port', '4444')
+    remote_url = 'None' if not args.ip else 'http://{}:{}/wd/hub'\
+        .format(args.ip, port)
+
+    desired_capabilities = DesiredCapabilities.CHROME if \
+        args.browser == 'chrome' else 'None'
+
+    test = TESTS[args.suite]
+
+    test['runner'](test['worker_cmd'], test['tender_file_path'],
+                   test['auction_id'])
+
+    auction_worker_defaults = test['auction_worker_defaults']
     cli_args = [
         '-L',
         'TRACE',
@@ -37,6 +73,9 @@ def main():
         '-v', 'tender_file_path:{}'.format(test['tender_file_path']),
         '-v', auction_worker_defaults.format(CWD),
         '-v', 'auction_id:{}'.format(test['auction_id']),
+        '-v', 'BROWSER:{}'.format(args.browser),
+        '-v', 'remote_url:{}'.format(remote_url),
+        '-v', 'desired_capabilities:{}'.format(desired_capabilities),
         '-l', '{0}/logs/log_{1}'.format(CWD, args.suite),
         '-r', '{0}/logs/report_{1}'.format(CWD, args.suite),
         '-P', test['suite'],
