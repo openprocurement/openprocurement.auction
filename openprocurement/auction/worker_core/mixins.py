08d26690aa9a1ae48e74f54fb65016c362c6eda8
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-08-07 19:16:54 +0300

    Remove methods that not used in insider

--- openprocurement/auction/worker_core/mixins.py
+++ openprocurement/auction/worker_core/mixins.py
@@ -5,7 +5,7 @@ from dateutil.tz import tzlocal
 from yaml import safe_dump as yaml_dump
 
 from openprocurement.auction.utils import generate_request_id, make_request
-from openprocurement.auction.worker_core.constants import ROUNDS, TIMEZONE
+from openprocurement.auction.worker_core.constants import TIMEZONE
 from openprocurement.auction.worker_core.journal import (
     AUCTION_WORKER_API_AUDIT_LOG_APPROVED,
     AUCTION_WORKER_API_AUDIT_LOG_NOT_APPROVED,
@@ -16,7 +16,7 @@ LOGGER = logging.getLogger("Auction Worker")
 
 
 class RequestIDServiceMixin(object):
-    """ Simpel mixin class """
+    """ Simple mixin class """
     def generate_request_id(self):
         self.request_id = generate_request_id()
 
@@ -24,58 +24,10 @@ class RequestIDServiceMixin(object):
 class AuditServiceMixin(object):
     """ Mixin class to create, modify and upload audit documents"""
     def prepare_audit(self):
-        self.audit = {
-            "id": self.auction_doc_id,
-            "auctionId": self._auction_data["data"].get("auctionID", ""),
-            "auction_id": self.tender_id,
-            "items": self._auction_data["data"].get("items", []),
-            "timeline": {
-                "auction_start": {
-                    "initial_bids": []
-                }
-            }
-        }
-        if self.lot_id:
-            self.audit["lot_id"] = self.lot_id
-        for round_number in range(1, ROUNDS + 1):
-            self.audit['timeline']['round_{}'.format(round_number)] = {}
-
-    def approve_audit_info_on_bid_stage(self):
-        turn_in_round = self.current_stage - (
-            self.current_round * (self.bidders_count + 1) - self.bidders_count
-        ) + 1
-        round_label = 'round_{}'.format(self.current_round)
-        turn_label = 'turn_{}'.format(turn_in_round)
-        self.audit['timeline'][round_label][turn_label] = {
-            'time': datetime.now(tzlocal()).isoformat(),
-            'bidder': self.auction_document["stages"][self.current_stage].get('bidder_id', '')
-        }
-        if self.auction_document["stages"][self.current_stage].get('changed', False):
-            self.audit['timeline'][round_label][turn_label]["bid_time"] = self.auction_document["stages"][self.current_stage]['time']
-            self.audit['timeline'][round_label][turn_label]["amount"] = self.auction_document["stages"][self.current_stage]['amount']
-            if self.features:
-                self.audit['timeline'][round_label][turn_label]["amount_features"] = str(
-                    self.auction_document["stages"][self.current_stage].get("amount_features")
-                )
-                self.audit['timeline'][round_label][turn_label]["coeficient"] = str(
-                    self.auction_document["stages"][self.current_stage].get("coeficient")
-                )
+        raise NotImplementedError
 
     def approve_audit_info_on_announcement(self, approved={}):
-        self.audit['timeline']['results'] = {
-            "time": datetime.now(tzlocal()).isoformat(),
-            "bids": []
-        }
-        for bid in self.auction_document['results']:
-            bid_result_audit = {
-                'bidder': bid['bidder_id'],
-                'amount': bid['amount'],
-                'time': bid['time']
-            }
-            if approved:
-                bid_result_audit["identification"] = approved[bid['bidder_id']].get('tenderers', [])
-                bid_result_audit["owner"] = approved[bid['bidder_id']].get('owner', '')
-            self.audit['timeline']['results']['bids'].append(bid_result_audit)
+        raise NotImplementedError
 
     def upload_audit_file_with_document_service(self, doc_id=None):
         files = {'file': ('audit_{}.yaml'.format(self.auction_doc_id),
